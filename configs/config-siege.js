5895b1b75976cf50dea39e883dc7c37623f9e457
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2025-06-06 12:53:41 -1000

    configureyeye

---
+++ configs/config-siege.js
@@ -0,0 +1,39 @@
+let map = {
+    "MODE": "tdm",
+    "ISSIEGE": true, 
+    "ISSURVIVALMODE": false,
+    "serverName": "Boss Rush",//siege
+    "TEAM_AMOUNT": 1,
+    "MODE": "tdm",
+    "WIDTH": 4000,
+    "HEIGHT": 4000,
+    "X_GRID": 15,
+    "Y_GRID": 15,
+    "MAX_FOOD": 20,
+    "MAX_NEST_FOOD": 10,
+    "MAX_CRASHERS": 30,
+    "MAX_SANCS": 3,
+    "DO_BASE_DAMAGE": false,
+    "ROOM_SETUP": [                                                                                     //mid
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "roid", "roid", "roid", "roid", "roid", "roid", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "rock", "rock", "rock", "rock", "rock", "rock", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "nest", "norm", "norm", "norm", "norm", "bas1", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "nest", "norm", "norm", "norm", "norm", "bas1", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "norm", "norm", "norm", "norm", "norm", "norm", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "nest", "norm", "norm", "norm", "norm", "bas1", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "rock", "rock", "rock", "rock", "rock", "rock", "roid"],
+        ["bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bosp", "bas2", "roid", "roid", "roid", "roid", "roid", "roid", "roid"]
+    ],
+    "BOTS": 20,
+    "BORDER_FORCE": 0.025,
+    "MAXBOSSES": 20,
+    "MINBOSSES": 10
+}
+map
