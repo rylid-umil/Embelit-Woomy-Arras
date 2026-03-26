6023033500383a060a64d044b3722db77eb384ca
rylid-umil <richard.becker98@yahoo.com>
2026-03-25 22:24:58 -0700

    Object.entries

--- audio.js
+++ audio.js
@@ -84,20 +84,20 @@ global.getSetting = function (setting) {
 global.playDeathSound = function () {
     if (getSetting("funnyDeath") == "on") {
         let deathSounds = []
-        Object.keys(global.sfx).forEach(function (element) {
+        Object.entries(global.sfx).forEach(function (element) {
             if (element.type = "sound") {
                 deathSounds.push(element)
             }
         })
-        let s = deathSounds[global.rnd(0, deathSounds.length-1)];
-        if (s.src.paused) {s.src.currentTime = 0} else {s.src.play()}; // If sound is already playing, restart it.
+        let s = deathSounds[global.rnd(0, deathSounds.length-1)][2];
+        if (s.paused) {s.currentTime = 0} else {s.play()}; // If sound is already playing, restart it.
     };
 };
 global.stopAllAudio = function (stype) {
-    Object.keys(global.sounds).forEach(function (element) {
+    Object.entries(global.sounds).forEach(function (element) {
         // Check if a type is specified. if so, only stop the sound if its type matches the selected type. If it is null (unselected), stop all sounds.
-        if (stype === null ? true : element.type == stype) {
-            element.src.currentTime = element.src.duration; // end the audio - this causes the ended Event to be activated instead of the paused one.
+        if (stype === null ? true : element[2] == stype) {
+            element[2].currentTime = element[2].duration; // end the audio - this causes the ended Event to be activated instead of the paused one.
         }
     });
 };
